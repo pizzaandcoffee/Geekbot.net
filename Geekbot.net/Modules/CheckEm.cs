c82d6ec6668d22480869d0988c1527807c1daff2
Runebaas <runebaas@gmail.com>
2017-09-15 21:10:22 +0200

    Add Dub, Trips and Quads check to !checkem (please fix if you see this)

--- Geekbot.net/Modules/CheckEm.cs
+++ Geekbot.net/Modules/CheckEm.cs
@@ -21,10 +21,31 @@ namespace Geekbot.net.Modules
         {
             try
             {
-                var sb = new StringBuilder();
+                int number = rnd.Next(10000000, 99999999);
+                var dubtriqua = "";
 
+                var ns = GetIntArray(number);
+                Console.WriteLine(ns.Length);
+                if (ns[7] == ns[6])
+                {
+                    dubtriqua = "DUBS";
+                    if (ns[6] == ns[5])
+                    {
+                        dubtriqua = "TRIPS";
+                        if (ns[5] == ns[4])
+                        {
+                            dubtriqua = "QUADS";
+                        }
+                    }
+                }
+
+                var sb = new StringBuilder();
                 sb.AppendLine($"Check em {Context.User.Mention}");
-                sb.AppendLine($"**{rnd.Next(100000, 99999999)}**");
+                sb.AppendLine($"**{number}**");
+                if (!string.IsNullOrEmpty(dubtriqua))
+                {
+                    sb.AppendLine($":tada: {dubtriqua} :tada:");
+                }
                 sb.AppendLine(checkEmImages.GetRandomCheckEmPic());
 
                 await ReplyAsync(sb.ToString());
@@ -33,6 +54,19 @@ namespace Geekbot.net.Modules
             {
                 Console.WriteLine(e.Message);
             }
+         
+        }
+
+        private int[] GetIntArray(int num)
+        {
+            List<int> listOfInts = new List<int>();
+            while (num > 0)
+            {
+                listOfInts.Add(num % 10);
+                num = num / 10;
+            }
+            listOfInts.Reverse();
+            return listOfInts.ToArray();
         }
     }
 }
