c7285f8bd48749ad32a234caf821c731cbfb868a
Daan Boerlage <dboerlage@cimpress.com>
2017-09-25 15:03:32 +0200

    Reworking the dice once again

--- Geekbot.net/Modules/Dice.cs
+++ Geekbot.net/Modules/Dice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,51 +19,54 @@ namespace Geekbot.net.Modules
 
         [Command("dice", RunMode = RunMode.Async)]
         [Summary("Roll a dice.")]
-        public async Task RollCommand([Remainder] [Summary("diceType")] string diceType = "1d20")
+        public async Task RollCommand([Remainder] [Summary("diceType")] string inputString = "1d20")
         {
-            var splitedDices = diceType.Split("+");
-            var dices = new List<DiceTypeDto>();
-            var mod = 0;
-            foreach (var i in splitedDices)
-            {
-                var dice = toDice(i);
-                if (dice.sides != 0 && dice.times != 0)
-                {
-                    dices.Add(dice);
-                }
-                else if (dice.mod != 0)
-                {
-                    if (mod != 0)
-                    {
-                        await ReplyAsync("You can only have one mod");
-                        return;
-                    }
-                    mod = dice.mod;
-                }
-            }
+            // convert input into real objects, wihtout computation
+            var tokens = inputString.Split("+");
 
-            if (!dices.Any())
+            var computableTokens = tokens.Select(token => toComputation(token)).Where(t => t != null).ToList();
+
+            if (!computableTokens.Any())
             {
                 await ReplyAsync(
                     "That is not a valid dice, examples are: 1d20, 1d6, 2d6, 1d6+2, 1d6+2d8+1d20+6, etc...");
                 return;
             }
 
+            // get computed results
+            var computedResults = computableTokens.Select(die => new ComputedResult { Die = die, Roll = Compute(die) }).ToList();
+            
+            // validate mod location and duplication
+            // ?
+
+            // format computed results
+            var result = ConvertToDisplay(computedResults, Context.User.Mention);
+            await ReplyAsync(result);
+        }
+
+        public class ComputedResult
+        {
+            public int Roll { get; set; }
+            public DiceTypeDto Die { get; set; }
+        }
+
+        public string ConvertToDisplay(IList<ComputedResult> computedResults, string user)
+        {
             var rep = new StringBuilder();
-            rep.AppendLine($":game_die: {Context.User.Mention}");
+            rep.AppendLine($":game_die: {user}");
             rep.Append("**Result:** ");
             var resultStrings = new List<string>();
             var total = 0;
             var extraText = "";
-            foreach (var dice in dices)
+            foreach (var dice in computedResults)
             {
                 var results = new List<int>();
-                for (var i = 0; i < dice.times; i++)
+                for (var i = 0; i < dice.Die.times; i++)
                 {
-                    var roll = rnd.Next(1, dice.sides);
+                    var roll = dice.Roll;
                     total += roll;
                     results.Add(roll);
-                    if (roll == dice.sides)
+                    if (roll == dice.Die.sides)
                     {
                         extraText = "**Critical Hit!**";
                     }
@@ -71,30 +75,35 @@ namespace Geekbot.net.Modules
                         extraText = "**Critical Fail!**";
                     }
                 }
-                resultStrings.Add($"{dice.diceType} ({string.Join(",", results)})");
+                resultStrings.Add($"{dice.Die.diceType} ({string.Join(",", results)})");
             }
             rep.Append(string.Join(" + ", resultStrings));
-            if (mod != 0)
-            {
-                rep.Append($" + {mod}");
-                total += mod;
-            }
             rep.AppendLine();
             rep.AppendLine($"**Total:** {total}");
             if (extraText != "")
             {
                 rep.AppendLine(extraText);
             }
-            await ReplyAsync(rep.ToString());
+            return rep.ToString();
         }
 
-        private DiceTypeDto toDice(string dice)
+        private int Compute(DiceTypeDto token)
+        {
+            return rnd.Next(1, token.sides);
+        }
+
+        public DiceTypeDto toComputation(string dice)
         {
             var diceParts = dice.Split('d');
             if (diceParts.Length == 2
                 && int.TryParse(diceParts[0], out int times)
                 && int.TryParse(diceParts[1], out int max))
             {
+                if (max == 0 || times == 0)
+                {
+                    return null;
+                }
+                
                 return new DiceTypeDto()
                 {
                     diceType = dice,
@@ -110,11 +119,12 @@ namespace Geekbot.net.Modules
                     mod = mod
                 };
             }
-            return new DiceTypeDto();
+            
+            return null;
         }
     }
 
-    class DiceTypeDto
+    public class DiceTypeDto
     {
         public string diceType { get; set; }
         public int times { get; set; }
