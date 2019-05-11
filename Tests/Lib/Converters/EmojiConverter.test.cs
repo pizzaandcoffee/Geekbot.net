f9269b877cf4ae3e44dd2244f08de10dea0c9c1f
runebaas <runebaas@gmail.com>
2019-05-12 01:38:56 +0200

    Use typed TestData for unit tests

---
+++ Tests/Lib/Converters/EmojiConverter.test.cs
@@ -0,0 +1,94 @@
+﻿using Geekbot.net.Lib.Converters;
+using Xunit;
+
+namespace Tests.Lib.Converters
+{
+    public class EmojiConverterTest
+    {
+        public class NumberToEmojiTestDto
+        {
+            public int Number { get; set; }
+            public string Expected { get; set; }
+        }
+        
+        public static TestData<NumberToEmojiTestDto> NumberToEmojiTestData =>
+            new TestData<NumberToEmojiTestDto>
+            {
+                {
+                    "2",
+                    new NumberToEmojiTestDto
+                    {
+                        Number = 2,
+                        Expected = ":two:"
+                    }
+                },
+                {
+                    "10",
+                    new NumberToEmojiTestDto
+                    {
+                        Number = 10,
+                        Expected = "🔟"
+                    }
+                },
+                {
+                    "15",
+                    new NumberToEmojiTestDto
+                    {
+                        Number = 15,
+                        Expected = ":one::five:"
+                    }
+                },
+                {
+                    "null",
+                    new NumberToEmojiTestDto
+                    {
+                        Number = 0,
+                        Expected = ":zero:"
+                    }
+                }
+            };
+
+        [Theory, MemberData(nameof(NumberToEmojiTestData))]
+        public void NumberToEmoji(string testName, NumberToEmojiTestDto testData)
+        {
+            var emojiConverter = new EmojiConverter();
+            var result = emojiConverter.NumberToEmoji(testData.Number);
+            Assert.Equal(result, testData.Expected);
+        }
+        
+        public class TextToEmojiTestDto
+        {
+            public string Text { get; set; }
+            public string Expected { get; set; }
+        }
+
+        public static TestData<TextToEmojiTestDto> TextToEmojiTestData =>
+            new TestData<TextToEmojiTestDto>
+            {
+                {
+                    "Test",
+                    new TextToEmojiTestDto
+                    {
+                        Text = "test",
+                        Expected = ":regional_indicator_t: :regional_indicator_e: :regional_indicator_s: :regional_indicator_t: "
+                    }
+                },
+                {
+                    "Best3+?",
+                    new TextToEmojiTestDto
+                    {
+                        Text = "Best3+?",
+                        Expected = ":b: :regional_indicator_e: :regional_indicator_s: :regional_indicator_t: :three: :heavy_plus_sign: :question: "
+                    }
+                }
+            };
+
+        [Theory, MemberData(nameof(TextToEmojiTestData))]
+        public void TextToEmoji(string testName, TextToEmojiTestDto testData)
+        {
+            var emojiConverter = new EmojiConverter();
+            var result = emojiConverter.TextToEmoji(testData.Text);
+            Assert.Equal(result, testData.Expected);
+        }
+    }
+}
---
+++ Tests/Lib/Levels/LevelCalc.test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Geekbot.net.Lib.Levels;
+using Xunit;
+
+namespace Tests.Lib.Levels
+{
+    public class LevelCalcTest
+    {
+        public class LevelCalcTestDto
+        {
+            public int Messages { get; set; }
+            public int ExpectedLevel { get; set; }
+        }
+
+        public static TestData<LevelCalcTestDto> LevelCalcTestData =>
+            new TestData<LevelCalcTestDto>()
+            {
+                {
+                    "500",
+                    new LevelCalcTestDto
+                    {
+                        Messages = 500,
+                        ExpectedLevel = 13
+                    }
+                },
+                {
+                    "41659",
+                    new LevelCalcTestDto
+                    {
+                        Messages = 41659,
+                        ExpectedLevel = 55
+                    }
+                },
+                {
+                    "0",
+                    new LevelCalcTestDto
+                    {
+                        Messages = 0,
+                        ExpectedLevel = 1
+                    }
+                },
+                {
+                    "4000000",
+                    new LevelCalcTestDto
+                    {
+                        Messages = 4000000,
+                        ExpectedLevel = 101
+                    }
+                }
+            };
+
+        [Theory, MemberData(nameof(LevelCalcTestData))]
+        public void GetLevel(string testName, LevelCalcTestDto testData)
+        {
+            var levelCalc = new LevelCalc();
+            var result = levelCalc.GetLevel(testData.Messages);
+            Assert.Equal(result, testData.ExpectedLevel);
+        }
+    }
+}
