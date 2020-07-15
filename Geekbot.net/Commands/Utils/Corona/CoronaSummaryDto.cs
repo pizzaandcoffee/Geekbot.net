efed2f712048e904de30299d66477003cd1ac533
runebaas <runebaas@gmail.com>
2020-07-15 03:09:43 +0200

    Add the !corona command

---
+++ Geekbot.net/Commands/Utils/Corona/CoronaSummaryDto.cs
@@ -0,0 +1,9 @@
+namespace Geekbot.net.Commands.Utils.Corona
+{
+    public class CoronaSummaryDto
+    {
+        public decimal TotalConfirmed { get; set; }
+        public decimal TotalDeaths { get; set; }
+        public decimal TotalRecovered { get; set; }
+    }
+}
