ae1b28ff77cdca8d9f1344f1fa82f04a8a75c750
Daan Boerlage <d@boerlage.me>
2021-11-09 00:49:46 +0100

    Decouple the WebApi and the Bot and move the startup code into a new project

---
+++ src/Bot/BotStartup.cs
@@ -0,0 +1,117 @@
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Geekbot.Bot.Handlers;
+using Geekbot.Core;
+using Geekbot.Core.Database;
+using Geekbot.Core.GlobalSettings;
+using Geekbot.Core.GuildSettingsManager;
+using Geekbot.Core.Logger;
+using Geekbot.Core.ReactionListener;
+using Geekbot.Core.UserRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Geekbot.Bot;
+
+public class BotStartup
+{
+    private readonly IServiceCollection _serviceCollection;
+    private readonly GeekbotLogger _logger;
+    private readonly RunParameters _runParameters;
+    private readonly IGlobalSettings _globalSettings;
+    private DiscordSocketClient _client;
+
+    public BotStartup(IServiceCollection serviceCollection, GeekbotLogger logger, RunParameters runParameters, IGlobalSettings globalSettings)
+    {
+        _serviceCollection = serviceCollection;
+        _logger = logger;
+        _runParameters = runParameters;
+        _globalSettings = globalSettings;
+    }
+
+    public async Task Start()
+    {
+        _logger.Information(LogSource.Geekbot, "Connecting to Discord");
+        SetupDiscordClient();
+        await Login();
+        await _client.SetGameAsync(_globalSettings.GetKey("Game"));
+        _logger.Information(LogSource.Geekbot, $"Now Connected as {_client.CurrentUser.Username} to {_client.Guilds.Count} Servers");
+        
+        _logger.Information(LogSource.Geekbot, "Registering Gateway Handlers");
+        await RegisterHandlers();
+        
+        _logger.Information(LogSource.Geekbot, "Done and ready for use");
+        await Task.Delay(-1);
+    }
+    
+    private void SetupDiscordClient()
+    {
+        _client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            LogLevel = LogSeverity.Verbose,
+            MessageCacheSize = 1000,
+            ExclusiveBulkDelete = true
+        });
+            
+        var discordLogger = new DiscordLogger(_logger);
+        _client.Log += discordLogger.Log;
+    }
+    
+    private async Task Login()
+    {
+        try
+        {
+            var token = await GetToken();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            while (!_client.ConnectionState.Equals(ConnectionState.Connected)) await Task.Delay(25);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(LogSource.Geekbot, "Could not connect to Discord", e);
+            Environment.Exit(GeekbotExitCode.CouldNotLogin.GetHashCode());
+        }
+    }
+    
+    private async Task<string> GetToken()
+    {
+        var token = _runParameters.Token ?? _globalSettings.GetKey("DiscordToken");
+        if (string.IsNullOrEmpty(token))
+        {
+            Console.Write("Your bot Token: ");
+            var newToken = Console.ReadLine();
+            await _globalSettings.SetKey("DiscordToken", newToken);
+            await _globalSettings.SetKey("Game", "Ping Pong");
+            token = newToken;
+        }
+
+        return token;
+    }
+    
+    private async Task RegisterHandlers()
+    {
+        var applicationInfo = await _client.GetApplicationInfoAsync();
+
+        _serviceCollection.AddSingleton<DiscordSocketClient>(_client);
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+        
+        var commands = new CommandService();
+        await commands.AddModulesAsync(Assembly.GetAssembly(typeof(BotStartup)), serviceProvider);
+            
+        var commandHandler = new CommandHandler(serviceProvider.GetService<DatabaseContext>(), _client, _logger, serviceProvider, commands, applicationInfo, serviceProvider.GetService<IGuildSettingsManager>());
+        var userHandler = new UserHandler(serviceProvider.GetService<IUserRepository>(), _logger, serviceProvider.GetService<DatabaseContext>(), _client);
+        var reactionHandler = new ReactionHandler(serviceProvider.GetService<IReactionListener>());
+        var statsHandler = new StatsHandler(_logger, serviceProvider.GetService<DatabaseContext>());
+        var messageDeletedHandler = new MessageDeletedHandler(serviceProvider.GetService<DatabaseContext>(), _logger, _client);
+                    
+        _client.MessageReceived += commandHandler.RunCommand;
+        _client.MessageDeleted += messageDeletedHandler.HandleMessageDeleted;
+        _client.UserJoined += userHandler.Joined;
+        _client.UserUpdated += userHandler.Updated;
+        _client.UserLeft += userHandler.Left;
+        _client.ReactionAdded += reactionHandler.Added;
+        _client.ReactionRemoved += reactionHandler.Removed;
+        if (!_runParameters.InMemory) _client.MessageReceived += statsHandler.UpdateStats;
+    }
+}
---
+++ src/Startup/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.Text;
+using CommandLine;
+using Geekbot.Bot;
+using Geekbot.Core;
+using Geekbot.Core.Converters;
+using Geekbot.Core.Database;
+using Geekbot.Core.DiceParser;
+using Geekbot.Core.ErrorHandling;
+using Geekbot.Core.GlobalSettings;
+using Geekbot.Core.GuildSettingsManager;
+using Geekbot.Core.Highscores;
+using Geekbot.Core.KvInMemoryStore;
+using Geekbot.Core.Levels;
+using Geekbot.Core.Logger;
+using Geekbot.Core.Media;
+using Geekbot.Core.RandomNumberGenerator;
+using Geekbot.Core.ReactionListener;
+using Geekbot.Core.UserRepository;
+using Geekbot.Core.WikipediaClient;
+using Geekbot.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Sentry;
+using Constants = Geekbot.Core.Constants;
+
+//
+// Parse Parameters
+//
+RunParameters runParameters = null!;
+Parser.Default.ParseArguments<RunParameters>(args)
+    .WithParsed(e => runParameters = e)
+    .WithNotParsed(_ => Environment.Exit(GeekbotExitCode.InvalidArguments.GetHashCode()));
+
+//
+// Print Logo
+//
+var logo = new StringBuilder();
+logo.AppendLine(@"  ____ _____ _____ _  ______   ___ _____");
+logo.AppendLine(@" / ___| ____| ____| |/ / __ ) / _ \\_  _|");
+logo.AppendLine(@"| |  _|  _| |  _| | ' /|  _ \| | | || |");
+logo.AppendLine(@"| |_| | |___| |___| . \| |_) | |_| || |");
+logo.AppendLine(@" \____|_____|_____|_|\_\____/ \___/ |_|");
+logo.AppendLine($"Version {Constants.BotVersion()} ".PadRight(41, '='));
+Console.WriteLine(logo.ToString());
+
+//
+// Init Logger
+//
+var logger = new GeekbotLogger(runParameters);
+logger.Information(LogSource.Geekbot, "Starting...");
+
+//
+// Connect to Database
+//
+logger.Information(LogSource.Geekbot, "Connecting to Database");
+var databaseInitializer = new DatabaseInitializer(runParameters, logger);
+var database = databaseInitializer.Initialize();
+database.Database.EnsureCreated();
+if(!runParameters.InMemory) database.Database.Migrate();
+var globalSettings = new GlobalSettings(database);
+
+//
+// Register Services
+//
+logger.Information(LogSource.Geekbot, "Registering Services");
+RegisterSentry();
+var serviceProvider = RegisterServices();
+
+//
+// Start Gateway Bot
+//
+if (!runParameters.DisableGateway)
+{
+    new BotStartup(serviceProvider, logger, runParameters, globalSettings).Start();
+}
+
+//
+// Start WebApi
+//
+if (!runParameters.DisableApi)
+{
+    WebApiStartup.StartWebApi(serviceProvider.BuildServiceProvider(), logger, runParameters, databaseInitializer.Initialize(), globalSettings);
+}
+
+ServiceCollection RegisterServices()
+{
+    var services = new ServiceCollection();
+    var userRepository = new UserRepository(databaseInitializer.Initialize(), logger);
+    var reactionListener = new ReactionListener(databaseInitializer.Initialize());
+    var guildSettingsManager = new GuildSettingsManager(databaseInitializer.Initialize());
+    var fortunes = new FortunesProvider(logger);
+    var levelCalc = new LevelCalc();
+    var emojiConverter = new EmojiConverter();
+    var mtgManaConverter = new MtgManaConverter();
+    var wikipediaClient = new WikipediaClient();
+    var randomNumberGenerator = new RandomNumberGenerator();
+    var mediaProvider = new MediaProvider(logger, randomNumberGenerator);
+    var kvMemoryStore = new KvInInMemoryStore();
+    var errorHandler = new ErrorHandler(logger, runParameters, () => Geekbot.Core.Localization.Internal.SomethingWentWrong);
+    var diceParser = new DiceParser(randomNumberGenerator);
+    
+    services.AddSingleton<IUserRepository>(userRepository);
+    services.AddSingleton<IGeekbotLogger>(logger);
+    services.AddSingleton<ILevelCalc>(levelCalc);
+    services.AddSingleton<IEmojiConverter>(emojiConverter);
+    services.AddSingleton<IFortunesProvider>(fortunes);
+    services.AddSingleton<IMediaProvider>(mediaProvider);
+    services.AddSingleton<IMtgManaConverter>(mtgManaConverter);
+    services.AddSingleton<IWikipediaClient>(wikipediaClient);
+    services.AddSingleton<IRandomNumberGenerator>(randomNumberGenerator);
+    services.AddSingleton<IKvInMemoryStore>(kvMemoryStore);
+    services.AddSingleton<IGlobalSettings>(globalSettings);
+    services.AddSingleton<IErrorHandler>(errorHandler);
+    services.AddSingleton<IDiceParser>(diceParser);
+    services.AddSingleton<IReactionListener>(reactionListener);
+    services.AddSingleton<IGuildSettingsManager>(guildSettingsManager);
+    services.AddTransient<IHighscoreManager>(e => new HighscoreManager(databaseInitializer.Initialize(), userRepository));
+    services.AddTransient(e => databaseInitializer.Initialize());
+
+    return services;
+}
+
+void RegisterSentry()
+{
+    var sentryDsn = runParameters.SentryEndpoint;
+    if (string.IsNullOrEmpty(sentryDsn)) return;
+    SentrySdk.Init(o =>
+    {
+        o.Dsn = sentryDsn;
+        o.Release = Constants.BotVersion();
+        o.Environment = "Production";
+        o.TracesSampleRate = 1.0;
+    });
+    logger.Information(LogSource.Geekbot, $"Command Errors will be logged to Sentry: {sentryDsn}");
+}
